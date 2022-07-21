7b105e7df072f5e468e5272872582ac1f8973313
Yudha Setyaji <setyajiyudha299@gmail.com>
2022-07-22 01:58:42 +0700

    Initial Commit

---
+++ post app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  post app
+//
+//  Created by Yudha Setyaji on 2022/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
